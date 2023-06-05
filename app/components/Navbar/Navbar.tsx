e2b3985e47b3967a8c8623d3bf9e9f71e9ce4108
Raghav Sharma <73748243+peregrine-falcon@users.noreply.github.com>
2023-06-05 15:25:32 +0530

    ;;;;

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -33,7 +33,7 @@ const Navbar = () => {
     return (
         <Disclosure as="nav" className="navbar">
             <>
-                <div className="mx-auto max-w-7xl px-6 py-4 lg:px-8">
+                <div className="mx-auto max-w-7xl px-6 lg:py-4 lg:px-8">
                     <div className="relative flex h-20 items-center justify-between">
                         <div className="flex flex-1 items-center sm:items-stretch sm:justify-start">
 
